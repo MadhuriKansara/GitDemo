25e157df706f1412d18101aec73198cb3965539d
MadhuriKansara <madhurikkansara@gmail.com>
2021-07-07 22:25:46 -0400

    This is commit from Asian team

--- src/test/java/mavenProject/mavenJava/RestAPITest.java
+++ src/test/java/mavenProject/mavenJava/RestAPITest.java
@@ -7,6 +7,9 @@ public class RestAPITest {
 	public void PostJira()
 	{
 		System.out.println("PostJira");
+		System.out.println("PostJira");
+		System.out.println("PostJira");
+		System.out.println("PostJira");
 	}
 	
 	@Test
